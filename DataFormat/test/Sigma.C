1d0e9aac6772a0e82a49100cbdac68320336e2d9
David Sheffield <sheffield@physics.rutgers.edu>
2014-12-02 12:32:07 -0600

    Added function to find effective sigma from @andjuo.

---
+++ DataFormat/test/Sigma.C
@@ -0,0 +1,97 @@
+//******************************************//
+// author: Chris Seez
+//
+//  This function evaluates the effective
+//  sigma of a histogram.
+//
+//******************************************//
+
+Double_t calc_effSigma(TH1 * hist)
+{
+  using std::cout;
+  using std::endl;
+
+  TAxis *xaxis = hist->GetXaxis();
+  Int_t nb = xaxis->GetNbins();
+  if(nb < 10) {
+    cout << "effsigma: Not a valid histo. nbins = " << nb << endl;
+    return 0.;
+  }
+
+  Double_t bwid = hist->GetBinWidth(1);
+  if(bwid == 0) {
+    cout << "effsigma: Not a valid histo. bwid = " << bwid << endl;
+    return 0.;
+  }
+  //Double_t xmax = xaxis->GetXmax(); // not used
+  Double_t xmin = xaxis->GetXmin();
+  Double_t ave = hist->GetMean();
+  Double_t rms = hist->GetRMS();
+
+  Double_t total=0.;
+  for(Int_t i=0; i<nb+2; i++) {
+    total+=hist->GetBinContent(i);
+  }
+  if(total < 10.) {
+    cout << "effsigma: Too few entries " << total << endl;
+    return 0.;
+  }
+  Int_t ierr=0;
+  Int_t ismin=999;
+
+  Double_t rlim=0.683*total;
+  Int_t nrms= Int_t(2.0*rms/(bwid));    // Set scan size to +/- rms
+  //if(nrms > nb/10) nrms=nb/10; // Could be tuned...
+
+  Double_t widmin=9999999.;
+  for(Int_t iscan=-nrms;iscan<nrms+1;iscan++) { // Scan window centre
+    Int_t ibm=Int_t((ave-xmin)/bwid+1+iscan);
+    Double_t x=(ibm-0.5)*bwid+xmin;
+    Double_t xj=x;
+    Double_t xk=x;
+    Int_t jbm=ibm;
+    Int_t kbm=ibm;
+
+    // ibm -- curent bin value
+    // x, xj, xk -- value of the x-axis
+    // jbm, kbm -- bin values
+
+    Double_t bin=hist->GetBinContent(ibm);
+    total=bin;
+    for(Int_t j=1;j<nb;j++){
+      if(jbm < nb) {
+        jbm++;
+        xj+=bwid;
+        bin=hist->GetBinContent(jbm);
+        total+=bin;
+        if(total > rlim) break;
+      }
+      else ierr=1;
+
+      if(kbm > 0) {
+        kbm--;
+        xk-=bwid;
+        bin=hist->GetBinContent(kbm);
+        total+=bin;
+        if(total > rlim) break;
+      }
+      else ierr=1;
+    }
+    Double_t dxf=(total-rlim)*bwid/bin;
+    Double_t wid=(xj-xk+bwid-dxf)*0.5;
+
+    if(wid < widmin) {
+      widmin=wid;
+      ismin=iscan;
+    }
+  }
+  if(ismin == nrms || ismin == -nrms) {
+    cout << endl << endl << "CRAP!!!" << endl;
+    cout << ismin << '\t' << nrms << endl;
+    ierr=3;
+  }
+  if(ierr != 0 && ierr != 1) cout << "effsigma: Error of type " << ierr <<endl;
+
+  return widmin;
+
+}
